e7fcb85d12067b0de258a7d852b298b7410cc49c
MichalHorejs <47171877+MichalHorejs@users.noreply.github.com>
2024-06-18 23:00:36 +0200

    Unused imports

--- src/main/java/com/example/tennisapp/models/Reservation.java
+++ src/main/java/com/example/tennisapp/models/Reservation.java
@@ -1,6 +1,5 @@
 package com.example.tennisapp.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
